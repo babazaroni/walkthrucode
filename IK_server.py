6ea0b22594c03016b0028123cb1432d9ba2f1e71
chuck <chuck@sanna.com>
2017-09-14 13:03:17 -0700

    added q values to DH_Table

--- IK_server.py
+++ IK_server.py
@@ -40,12 +40,12 @@ def handle_calculate_IK(req):
         # Modified DH params
 
         DH_Table = {
-            alpha0: 0, a0: 0, d1: 0.75,
-            alpha1: -pi / 2.0, a1: 0.35, d2: 0, q2: q2 - pi / 2.0,
-            alpha2: 0, a2: 1.25, d3: 0,
-            alpha3: -pi / 2.0, a3: -0.054, d4: 1.50,
-            alpha4: pi / 2.0, a4: 0, d5: 0,
-            alpha5: -pi / 2.0, a5: 0, d6: 0,
+            alpha0: 0, a0: 0, d1: 0.75, q1: q1,
+            alpha1: -pi / 2.0, a1: 0.35, d2: 0, q2:- pi / 2.0 + q2,
+            alpha2: 0, a2: 1.25, d3: 0, q3: q3,
+            alpha3: -pi / 2.0, a3: -0.054, d4: 1.50, q4: q4,
+            alpha4: pi / 2.0, a4: 0, d5: 0, q5: q5,
+            alpha5: -pi / 2.0, a5: 0, d6: 0,q6: q6,
             alpha6: 0, a6: 0, d7: 0.303, q7: 0}
 
         # Define Modified DH Transformation matrix
